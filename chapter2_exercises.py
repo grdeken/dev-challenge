fa910262005ae31896678e9e46c8eac156907b0f
grdeken <grdeken@gmail.com>
2013-09-26 20:04:55 -0400

    Chapter 2 Homework

--- chapter2_exercises.py
+++ chapter2_exercises.py
@@ -1,3 +1,17 @@
 # Exercises for chapter 2:
 
-# Name:
+# Name: Grant Deken
+
+# 2.2 -- Integers starting with '0' are represented in base 8 (octal numbers).
+
+# 2.3 --
+#  1. width/2 = 8
+#  2. width/2.0 = 8.5
+#  3. height/3 = 4.0
+#  4. 1 + 2 * 5 = 11
+#  5. delimeter * 5 = '.....' 
+
+# 2.4
+#  1. 523.5987755982989
+#  2. $946.19
+#  3. 7:21:15 AM
