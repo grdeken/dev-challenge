ba0d26440bc30010e6a8ea6452ecc170b8593302
grdeken <grdeken@gmail.com>
2013-10-01 01:19:21 -0400

    Chapters 2 and 3. Not yet completed

--- chapter3_exercises.py
+++ chapter3_exercises.py
@@ -1,3 +1,29 @@
 # Exercises for chapter 3:
 
-# Name:
+# Name: Grant Deken
+
+# 3.3 --
+#     def right_justify(s):
+#     print (' '*(70-len(s))+s)
+#
+# Then call right_justify('allen')
+
+# 3.4 --
+#   1.  def do_twice(a, bee):
+#           a(bee)
+#	    a(bee)
+#   2.  def print_twice(bee):
+#	    print bee
+#	    pring bb
+#   3.  do_twice(print_twice, 'bee')
+#   4.  def do_four(a, bee):
+#	    do_twice(a, bee)
+#	    do_twice(a, bee)
+#	do_four(print_twice, 'bee')
+
+
+# 3.5 -- 
+#	
+#
+#
+
